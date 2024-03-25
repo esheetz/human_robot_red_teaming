48d37b30cf7dd7f13da44a7a384704061da8375a
Emily Sheetz <ejsheetz@yahoo.com>
2024-03-25 11:21:07 -0500

    refactor YAML formatting checks for less repetition

--- scripts/yaml_formatting_checks.py
+++ scripts/yaml_formatting_checks.py
@@ -7,17 +7,52 @@ import os
 
 from likelihood_consequence_risk import LikelihoodLevels, ConsequenceClasses
 
-###########################
-### YAML FILE EXISTENCE ###
-###########################
+###################################
+### YAML FILE FORMATTING CHECKS ###
+###################################
 
 class YAMLChecks:
+
+    # CHECK YAML FILE EXISTENCE
 
     @staticmethod
     def check_yaml_existence(yaml_file_path):
         # check if given YAML file exists and is a file
         return (os.path.exists(yaml_file_path) and
                 os.path.isfile(yaml_file_path))
+
+    # CHECK YAML FORMATTING
+
+    @staticmethod
+    def check_yaml_formatting(file_nickname, yaml_dict, env_name, list_key, list_elem_keys):
+        # check if environment exists
+        if env_name not in yaml_dict.keys():
+            print("ERROR: environment " + env_name + " does not exist in " + file_nickname + " file")
+            return False
+
+        # check for list key
+        if list_key not in yaml_dict[env_name].keys():
+            print("ERROR: environment " + env_name + " has no " + file_nickname + "s defined under key '" + list_key + "'")
+            return False
+
+        # get number of elements in list
+        num_elems = len(yaml_dict[env_name][list_key])
+
+        # initialize valid elements flag
+        valid_elems = True
+
+        # check each element in list
+        for i in range(num_elems):
+            # get element
+            elem = yaml_dict[env_name][list_key][i]
+
+            # check for each key in list element
+            for elem_key in list_elem_keys:
+                if elem_key not in elem.keys():
+                    print("ERROR: " + file_nickname + " " + str(i) + " of " + str(num_elems) + " does not have a " + elem_key)
+                    valid_elems = False
+
+        return valid_elems
 
 
 
@@ -29,43 +64,11 @@ class YAMLStateSpaceChecks(YAMLChecks):
 
     @staticmethod
     def check_risky_condition_yaml_formatting(yaml_dict, env_name):
-        # check if environment exists
-        if env_name not in yaml_dict.keys():
-            print("ERROR: environment " + env_name + " does not exist in risky conditions file")
-            return False
-
-        # check for list of conditions
-        if 'conditions' not in yaml_dict[env_name].keys():
-            print("ERROR: environment " + env_name + " has no risky conditions defined under key 'conditions'")
-            return False
-
-        # get number of conditions
-        num_conds = len(yaml_dict[env_name]['conditions'])
-
-        # initialize valid condition flag
-        valid_conditions = True
-
-        # check each condition in list
-        for i in range(num_conds):
-            # get condition
-            condition = yaml_dict[env_name]['conditions'][i]
-
-            # check for name
-            if 'name' not in condition.keys():
-                print("ERROR: risky condition " + str(i) + " of " + str(num_conds) + " does not have a name")
-                valid_conditions = False
-
-            # check for likelihood
-            if 'likelihood' not in condition.keys():
-                print("ERROR: risky condition " + str(i) + " of " + str(num_conds) + " does not have a likelihood value")
-                valid_conditions = False
-
-            # check for consequence
-            if 'consequence' not in condition.keys():
-                print("ERROR: risky condition " + str(i) + " of " + str(num_conds) + " does not have a consequence value")
-                valid_conditions = False
-
-        return valid_conditions
+        return YAMLChecks.check_yaml_formatting(file_nickname="risky condition",
+                                                yaml_dict=yaml_dict,
+                                                env_name=env_name,
+                                                list_key="conditions",
+                                                list_elem_keys=["name","likelihood","consequence"])
 
     @staticmethod
     def check_valid_risky_condition_values(cond_dict, i, num_conds):
@@ -98,33 +101,11 @@ class YAMLActionSpaceChecks(YAMLChecks):
 
     @staticmethod
     def check_risk_mitigating_action_yaml_formatting(yaml_dict, env_name):
-        # check if environment exists
-        if env_name not in yaml_dict.keys():
-            print("ERROR: environment " + env_name + " does not exist in risk mitigating actions file")
-            return False
-
-        # check for list of actions
-        if 'actions' not in yaml_dict[env_name].keys():
-            print("ERROR: environment " + env_name + " has no risk mitigating actions defined under key 'actions'")
-            return False
-
-        # get number of actions
-        num_acts = len(yaml_dict[env_name]['actions'])
-
-        # initialize valid action flag
-        valid_actions = True
-
-        # check each action in list
-        for i in range(num_acts):
-            # get action
-            action = yaml_dict[env_name]['actions'][i]
-
-            # check for name
-            if 'name' not in action.keys():
-                print("ERROR: risk mitigating action " + str(i) + " of " + str(num_acts) + " does not have a name")
-                valid_actions = False
-
-        return valid_actions
+        return YAMLChecks.check_yaml_formatting(file_nickname="risk mitigating action",
+                                                yaml_dict=yaml_dict,
+                                                env_name=env_name,
+                                                list_key="actions",
+                                                list_elem_keys=["name"])
 
     @staticmethod
     def check_valid_risk_mitigating_action_values(act_dict, i, num_acts):
@@ -145,5 +126,16 @@ class YAMLActionSpaceChecks(YAMLChecks):
 ########################################
 
 class YAMLPolicyStarterChecks(YAMLChecks):
-    pass
-    # TODO
+
+    @staticmethod
+    def check_risk_mitigating_policy_starter_yaml_formatting(yaml_dict, env_name):
+        return YAMLChecks.check_yaml_formatting(file_nickname="risk mitigating policy starter",
+                                                yaml_dict=yaml_dict,
+                                                env_name=env_name,
+                                                list_key="policy_starter",
+                                                list_elem_keys=["conditions", "action"])
+
+    @staticmethod
+    def check_valid_risk_mitigating_policy_starter_values(pol_dict, i, num_pols):
+        # TODO
+        return False
